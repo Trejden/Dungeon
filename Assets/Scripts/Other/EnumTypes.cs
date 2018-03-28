fb316a926baa7ca3c4077b4b43664ffb686f24f5
Makulak <j.makulak@gmail.com>
2018-03-28 13:19:01 +0200

    Adding enum types

---
+++ Assets/Scripts/Other/EnumTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Other
+{
+    public enum RoomType { NoRoom, Normal, Boss, Shop, Hidden, Locked, Treasure }
+    public enum ConnectionType { NoConnection, Normal, Hidden, Locked }
+    public enum Direction { Left, Up, Right, Down };
+}
